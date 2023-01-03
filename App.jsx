cc8ed495c66775e2909a9c8b399b1d9093952abb
kazuki <106413890+kazuki-ok@users.noreply.github.com>
2023-01-03 16:38:51 +0900

    create memoeditscreen

--- App.jsx
+++ App.jsx
@@ -3,9 +3,11 @@ import { StyleSheet } from 'react-native';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoDetailScreen />
+    <MemoCreateScreen />
   );
 }
